ec402be5f35fb61b5cfb8efd5432a09372033746
sparlund <johan.sparlund@gmail.com>
2018-10-05 22:44:20 +0200

    Small updates

--- radiotreby.py
+++ radiotreby.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy.util as utills
+import spotipy.util as util
 
 cid 		= '2dbb1a45a22249aea988931d9d8bc0c6' 
 secret 		= '6c90bbf62d8d467fb280f54b4dae7211'
@@ -13,34 +13,44 @@ token 		= util.prompt_for_user_token(username,scope,client_id=cid,client_secret=
 sp 			= spotipy.Spotify(auth=token)
 
 
-
+# Website to crawl songs from
 r = requests.get('http://www.radiotreby.se/')
-s = BeautifulSoup(r.content, 'html.parser')
+s = BeautifulSoup(r.content, 'html.parser') 
+# This is the box where the latest played songs are found
 senasts_spelat = s.find("span", id="playlist")
+# Do some text manipulations to extract song names and artist
 song_list = senasts_spelat.text.split('\n')
-# Delete last item, it'll just be an empty entry.
 song_list.pop()
-song_list = [a[6:] for a in song_list]
-
+song_list = [a[6:] for a in song_list] # Remove time stamp!
 songs = list()
 for song in song_list:
+	# Split to artist and song
 	artist,song_name = song.split('-')
+	# Remove whitespace characters
 	artist = artist.rstrip(' ').strip(' ')	
-	song_name = song_name.rstrip(' ')
+	song_name = song_name.rstrip(' ').strip(' ')
 	songs.append({'artist': artist, 'song': song_name})
 
-# See which songs already are in the list:
-#radio_treby_list = sp.user_playlists(user, limit=50, offset=0)
-list_uri = 'spotify:user:sparlund:playlist:2edTLPX9eFi1a9Dgr8EqW0'
+# Check if a playlist called "Radio Treby" already exists
+# If it exists, we just need to find the id of it.
+# Other we need to create a playlist.
+sp_playlist_info = sp.user_playlists(username, limit=50, offset=0)
+playlists = sp_playlist_info['items']
+for counter,list_name in enumerate(playlists):
+	if list_name == 'Radio Treby':
+		list_uri = playlists['items'][counter]['id']
+		break
+	if counter+1 == len(playlists):
+		created_playlist = sp.user_playlist_create(username, 'Radio Treby', public=True, description='')
+		list_uri = created_playlist['uri']	
 # Want to see which songs are already in the list so that we don't add duplicates
-radio_treby_list = sp.user_playlist(username, playlist_id = list_uri, fields=['tracks'])
-tracks_in_radio_treby_list = list()
-for track in radio_treby_list['tracks']['items']: 
-	tracks_in_radio_treby_list.append(track['track']['uri'])
-tracks_in_radio_treby_list = set(tracks_in_radio_treby_list)
-#sp.user_playlist_create(username, 'Radio Treby', public=True, description='')
+songs_in_playlist = sp.user_playlist(username, playlist_id = list_uri, fields=['tracks'])
+tracks_2_add = list()
+for track in songs_in_playlist['tracks']['items']: 
+	tracks_2_add.append(track['track']['uri'])
 # Want to search for the songs we scraped for and add to the playlist:
 for j,song in enumerate(songs):
+	# Create the search string
 	if ',' in song['artist']:
 		artist_search_string = ''
 		for counter,individual_artist in enumerate(song['artist'].split(',')):
@@ -50,16 +60,14 @@ for j,song in enumerate(songs):
 	else:
 		artist_search_string = '\"'+song['artist']+'\"'		
 
-
 	search_string = 'artist:' + artist_search_string + ' track:' + '\"'+song['song']+'\"'
+	# Perform search
 	results = sp.search(q=search_string, type='track',limit=1)
-	print(search_string)
 	# Check if a song is found:
 	if results['tracks']['items']:
 		# Save the track's id
 		track_id = results['tracks']['items'][0]['uri']	
-		if track_id not in tracks_in_radio_treby_list:
-			print('track added: '+track_id)
+		if track_id not in tracks_2_add:
 			sp.user_playlist_add_tracks(username,list_uri,[track_id],position=None)
 		else:
 			print('track not added bc already in list')
